bf219cb35c007744d22709cdf42182d4bee3b2e9
morihiroto <arigatoeggplant@gmail.com>
2022-05-26 18:39:25 +0900

    loginとsignUpのロジックの当たりをつけて仮に作成

--- app/src/main/java/com/hirogram/chatapplication/SignUp.kt
+++ app/src/main/java/com/hirogram/chatapplication/SignUp.kt
@@ -2,10 +2,36 @@ package com.hirogram.chatapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUp : AppCompatActivity() {
+
+    private lateinit var edtName: EditText
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnSignUp: Button
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        edtName = findViewById(R.id.edt_name)
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            //signUp(email, password)
+        }
+    }
+
+    private fun singUp(email: String, password: String) {
+        //logic of create users
     }
 }
